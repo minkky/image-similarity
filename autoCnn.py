100395c437d840c58cfdb853aa66c638df02953b
minkky <minjiseo95@gmail.com>
2019-03-26 17:47:55 +0900

    txt deleted, autoCnn test result added

---
+++ autoCnn.py
@@ -0,0 +1,112 @@
+import keras
+import random, os
+from keras.layers import Activation, Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Flatten
+from keras.models import Model, Sequential
+from keras import backend as K
+from glob import glob
+from PIL import Image
+import numpy as np
+import re
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+import matplotlib as mpl
+import matplotlib.pylab as plt
+
+batch_size = 128
+num_classes = 6
+epochs = 1000
+
+img_rows, img_cols = 28, 28
+label = ["circle", "square", "triangle", "horizontal", "vertical", "diagonal"]
+
+def get_datasets_labels(path):
+	files = os.listdir(path)
+	labels = []
+	sets = []
+	for file in files:
+		pic = Image.open(path + file)
+		lb = file.replace(re.findall("\d+", file)[0], '').replace('.png', '')
+		labels.append(label.index(lb))
+		pic = np.array(pic)
+		sets.append(pic)
+	return sets, labels
+
+def load_data():
+    tr_path = 'trains/'
+    te_path = 'tests/'
+
+    (tr_sets, tr_labels) = get_datasets_labels(tr_path)
+    (te_sets, te_labels) = get_datasets_labels(te_path)
+
+    tr_sets = np.array(tr_sets)
+    te_sets = np.array(te_sets)
+    tr_labels = np.array(tr_labels)
+    te_labels = np.array(te_labels)
+    return tr_sets, tr_labels, te_sets, te_labels
+
+(x_train, y_train, x_test, y_test) = load_data()
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255.
+x_test /= 255.
+
+input_img = Input(shape=(28, 28, 1)) 
+
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = Dropout(0.25)(x)
+x = Dense(8)(x)
+x = Dropout(0.25)(x)
+#x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+#x = Conv2D(16, (3, 3), activation='relu')(x)
+#x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)\
+
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')#, metrics=['accuracy'])
+
+print(x_train.shape)
+
+autoencoder.fit(x_train, x_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1, validation_data=(x_test, x_test))
+
+#score = model.evaluate(x_test, x_test, verbose=0)
+#print('Test loss:', score[0])
+#print('Test accuracy:', score[1])
+
+decoded_imgs = autoencoder.predict(x_test)
+n = 30
+plt.figure(figsize=(20, 4))
+for i in range(1, n):
+    # display original
+    ax = plt.subplot(2, n, i)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
--- cnn.py
+++ cnn.py
@@ -1,43 +1,60 @@
 import keras
 import random, os
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Flatten
+from keras.layers import Activation, Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Flatten
 from keras.models import Model, Sequential
 from keras import backend as K
 from glob import glob
 from PIL import Image
 import numpy as np
 import re
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+
 
 batch_size = 128
 num_classes = 6
-epochs = 1000
+epochs = 500
 
 img_rows, img_cols = 28, 28
 label = ["circle", "square", "triangle", "horizontal", "vertical", "diagonal"]
 
-def load_data():
-    path = 'trains/'
-    dataset = []
-    filenames = os.listdir(path)
-    index = 0
-    labels = []
-    for file in filenames:
-        pic = Image.open(path + file)
-        lb = file.replace(re.findall("\d+", file)[0], '').replace('.png', '')
-        labels.append(label.index(lb))
-        pic = np.array(pic)
-        dataset.append(pic)
-    dataset = np.array(dataset)
-    labels = np.array(labels)
-    return dataset, labels
+def get_datasets_labels(path):
+	files = os.listdir(path)
+	labels = []
+	sets = []
+	for file in files:
+		pic = Image.open(path + file)
+		lb = file.replace(re.findall("\d+", file)[0], '').replace('.png', '')
+		labels.append(label.index(lb))
+		pic = np.array(pic)
+		sets.append(pic)
+	return sets, labels
 
-(x_train, y_train) = load_data()
+def load_data():
+    tr_path = 'trains/'
+    te_path = 'tests/'
+
+    (tr_sets, tr_labels) = get_datasets_labels(tr_path)
+    (te_sets, te_labels) = get_datasets_labels(te_path)
+
+    tr_sets = np.array(tr_sets)
+    te_sets = np.array(te_sets)
+    tr_labels = np.array(tr_labels)
+    te_labels = np.array(te_labels)
+    return tr_sets, tr_labels, te_sets, te_labels
+
+(x_train, y_train, x_test, y_test) = load_data()
 y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 x_train = x_train.astype('float32')
-x_train /= 255
+x_test = x_test.astype('float32')
+x_train /= 255.
+x_test /= 255.
+
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -45,11 +62,13 @@ model.add(Conv2D(32, kernel_size=(3, 3),
                  input_shape=input_shape))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Activation('relu'))
+#model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.25))
 model.add(Dense(num_classes, activation='softmax'))
+
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
@@ -57,4 +76,10 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
-          verbose=1)
+          verbose=1, validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#SVG(model_to_dot(model, show_shapes=True).create(prog='dot', format='svg'))
