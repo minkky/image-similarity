38ca25e10222ff3a4581623ac937efdfa56ffee6
minkky <minjiseo95@gmail.com>
2019-04-02 18:25:18 +0900

    added training/test/validation sets
    makeTrainingSet.py file modified ( fill or line draw shape)

--- makeTrainingSet.py
+++ makeTrainingSet.py
@@ -30,12 +30,12 @@ def addNoise():
 		picture = np.array(picture)
 		scipy.misc.imsave(savepath+file, picture)
 
-path = 'val/'
-savepath = 'vals/'
+path = './dataset/original/test/'
+savepath = './dataset/tests/'
 label = ["circle", "square", "triangle", "horizontal", "vertical", "diagonal"]
 
 for j in range(0, 6):
-	for i in range(0, 100):
+	for i in range(50, 100):
 		win = pg.display.set_mode((width, height))
 		win.fill(white)
 		if j == 0:
@@ -49,13 +49,15 @@ for j in range(0, 6):
 					sec = random.randint(2, 25)
 					third = random.randint(first, 27)
 					fourth = random.randint(sec, 27)
-				pg.draw.ellipse(win, (0, 0, 0), (first, sec, third, fourth))
+				#pg.draw.ellipse(win, (0, 0, 0), (first, sec, third, fourth))	
+				pg.draw.ellipse(win, (0, 0, 0), (first, sec, third, fourth), random.randint(1, 2))
 			else: 
 				center = (random.randint(2, 28), random.randint(2, 28))
 				radius = min([random.randint(2, 15), min(center)])
 				while (center[0] + radius > 28 or center[1] + radius > 28) and (center[0] - radius < 0 or center[1] - radius < 0):
 					center = (random.randint(2, 28), random.randint(2, 28))
-				pg.draw.circle(win, (0, 0, 0), center, radius, 0)		
+				#pg.draw.circle(win, (0, 0, 0), center, radius, 0)	
+				pg.draw.circle(win, (0, 0, 0), center, radius, random.randint(1, 2))
 		elif j == 1:
 			first = random.randint(1, 25)
 			sec = random.randint(1, 25)
@@ -66,24 +68,26 @@ for j in range(0, 6):
 				sec = random.randint(1, 25)
 				third = random.randint(first, 28)
 				fourth = random.randint(sec, 28)
-			pg.draw.rect(win, (0, 0, 0), (first, sec, third, fourth))
+			#pg.draw.rect(win, (0, 0, 0), (first, sec, third, fourth))
+			pg.draw.rect(win, (0, 0, 0), (first, sec, third, fourth), random.randint(1, 2))
 		elif j == 2:
 			top = (random.randint(1, 25), random.randint(1, 23))
 			h = random.randint(3, 13)
 			w = random.randint(5, 15)
 			left = (top[0] - w, top[1] +  h)
 			right = (top[0] + w, top[1] + h)
-			pg.draw.polygon(win, (0, 0, 0), [top, left, right])
+			#pg.draw.polygon(win, (0, 0, 0), [top, left, right])
+			pg.draw.polygon(win, (0, 0, 0), [top, left, right], random.randint(1, 2))
 		elif j == 3:
 			start = (random.randint(2, 25), random.randint(2, 25))
 			end = (random.randint(2, 27), start[1] + random.randint(0, 1)*random.choice([+1, -1])) # random.randint(0, 1)*random.choice([+1, -1])
-			while abs(start[0] - end[0]) < 5:
+			while abs(start[0] - end[0]) < 7:
 				end = (random.randint(2, 27), start[1] +random.randint(0, 1)*random.choice([+1, -1])) # random.randint(0, 1)*random.choice([+1, -1])
 			pg.draw.line(win, (0, 0, 0), start, end, random.randint(1, 3))
 		elif j == 4:
 			start = (random.randint(2, 25), random.randint(2, 25))
 			end = (start[0]+random.randint(0, 1)*random.choice([+1, -1]), random.randint(2, 27))
-			while abs(start[1] - end[1]) < 5 :
+			while abs(start[1] - end[1]) < 7 :
 				end = (start[0]+random.randint(0, 1)*random.choice([+1, -1]), random.randint(2, 27))
 			pg.draw.line(win, (0, 0, 0), start, end, random.randint(1, 3))
 		elif j == 5:
@@ -91,7 +95,7 @@ for j in range(0, 6):
 			while True:
 				start = (random.randint(1, 28), random.randint(1, 28))
 				end = (random.randint(1, 28), random.randint(1, 28))
-				if (abs(start[0] - end[0]) >= 8 and abs(start[1] - end[1]) >=8):
+				if (abs(start[0] - end[0]) >= 9 and abs(start[1] - end[1]) >=9):
 					break
 			pg.draw.line(win, (0, 0, 0), start, end, random.randint(1, 4))
 		fname = path + label[j] + str(i) + ".png"
